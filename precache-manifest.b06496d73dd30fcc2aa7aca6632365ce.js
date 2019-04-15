311576f3d0c1118a944c26120994a3e6f93aa3e4
ravibasil <ravi09basil@rediffmail.com>
2019-04-15 11:30:54 +0530

    Updates

---
+++ precache-manifest.b06496d73dd30fcc2aa7aca6632365ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  },
+  {
+    "revision": "a848b10ab41341f26175",
+    "url": "/me/static/js/main.12827f97.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/me/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a848b10ab41341f26175",
+    "url": "/me/static/css/main.59446ec6.chunk.css"
+  },
+  {
+    "revision": "94b51a58c7fd6b6b4653ad736a984943",
+    "url": "/me/index.html"
+  }
+];
